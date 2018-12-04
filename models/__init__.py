eb931d45b5daea55d2d5572c6058f9af123240de
WorkingTitus <workingtitus@gmail.com>
2018-12-04 00:20:50 +0000

    Added AuthToken table model.

--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,5 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 from .user import User
+from .auth import AuthToken
 from .account import Account
---
+++ models/auth.py
@@ -0,0 +1,21 @@
+# Own Imports
+from . import Base
+from config import AppConfig
+
+# Batteries
+from time import time
+from enum import Enum
+
+# Third Party Imports
+from sqlalchemy import Column, ForeignKey, Integer, String
+from secrets import token_hex
+
+
+class AuthToken(Base):
+
+    __tablename__ = 'auth_token'
+
+    user_id = Column('user_id', Integer, ForeignKey("user.user_id"), primary_key=True, nullable=False)
+    token_type = Column('token_type', Enum('bearer'), unique=True, nullable=False)
+    token = Column('token', String(64), unique=True, nullable=False)
+    expires_at = Column('expires_at', Integer, default=int(time() + AppConfig.TOKEN_LIFE))
