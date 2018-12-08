079ffc39da48347fde1e834a12c1ef5390357ac0
WorkingTitus <workingtitus@gmail.com>
2018-12-08 17:16:22 +0000

    Added email to the user model.

--- models/user.py
+++ models/user.py
@@ -15,5 +15,6 @@ class User(Base):
     user_id = Column('user_id', Integer, primary_key=True)
     account_id = Column('account_id', Integer, ForeignKey("account.account_id"), nullable=False)
     username = Column('username', String(16), unique=True, index=True, nullable=False)
+    email = Column('email', String(64), unique=True, nullable=False)
     password = Column('password', String(100), nullable=False)
     created_at = Column('created_at', Integer, default=int(time()))
