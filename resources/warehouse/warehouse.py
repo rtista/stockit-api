48bf9ac89668963e18d29ebf4acbefa608d231d7
WorkingTitus <workingtitus@gmail.com>
2018-12-29 20:48:24 +0000

    Warehouse update and delete available and properly working.

--- resources/warehouse/warehouse.py
+++ resources/warehouse/warehouse.py
@@ -1,5 +1,6 @@
 # Imports
 from models import Warehouse, UserWarehouse, AuthToken
+from hooks import Authorize
 
 # Batteries
 from time import time
@@ -9,38 +10,23 @@ import falcon
 from passlib.hash import pbkdf2_sha256
 from sqlalchemy.exc import SQLAlchemyError, NoReferenceError
 
-
+@falcon.before(Authorize())
 class WarehouseResource:
 
     def on_get(self, req, resp):
         """
-        Returns warehouse in the user account.
+        Handles GET requests.
+
+        Allows retrieving the warehouses of the respective user account.
 
         Args:
             req ([type]): The request object.
             resp ([type]): The response object.
         """
-        # TODO: Create Hook to handle this common necessity
-        # Read Authorization header
-        if req.auth == None:
-            raise falcon.HTTPBadRequest('Bad Request', 'No Authorization header provided.')
-
-        # Index 0: Authentication Type
-        # Index 1: Token
-        auth = req.auth.split(' ')
-
-        # Get user_id from valid token and auth_type
-        token = self.db_conn.query(AuthToken).filter(AuthToken.auth_type == auth[0],
-         AuthToken.token == auth[1], AuthToken.expires_at > int(time())).first()
-
-        # Check if token exists
-        if token == None:
-            raise falcon.HTTPUnauthorized('Unauthorized', 'Invalid authentication token.')
-
         warehouses = []
 
-        for warehouse in self.db_conn.query(Warehouse).filter(UserWarehouse.user_id == token.user_id).join(UserWarehouse, UserWarehouse.warehouse_id == Warehouse.warehouse_id):
-            warehouses.append({'name': warehouse.name, 'description': warehouse.description})
+        for warehouse in self.db_conn.query(Warehouse).filter(UserWarehouse.user_id == self.user_id).join(UserWarehouse, UserWarehouse.warehouse_id == Warehouse.warehouse_id):
+            warehouses.append({'name': warehouse.name, 'description': warehouse.description, 'latitude': warehouse.latitude, 'longitude': warehouse.longitude})
 
         resp.media = {'warehouses': warehouses}
         resp.status = falcon.HTTP_200
@@ -48,24 +34,14 @@ class WarehouseResource:
 
     def on_post(self, req, resp):
         """
-        Handle POST requests.
+        Handles POST requests.
+
+        Allows creating warehouse entities.
+
+        Args:
+            req ([type]): The request object.
+            resp ([type]): The response object.
         """
-        # Read Authorization header
-        if req.auth == None:
-            raise falcon.HTTPBadRequest('Bad Request', 'No Authorization header provided.')
-
-        # Index 0: Authentication Type
-        # Index 1: Token
-        auth = req.auth.split(' ')
-
-        # Get user_id from valid token and auth_type
-        token = self.db_conn.query(AuthToken).filter(AuthToken.auth_type == auth[0],
-         AuthToken.token == auth[1], AuthToken.expires_at > int(time())).first()
-
-        # Check if token exists
-        if token == None:
-            raise falcon.HTTPUnauthorized('Unauthorized', 'Invalid authentication token.')
-
         # Read warehouse details
         name = req.media.get('name')
         description = req.media.get('description')
@@ -86,7 +62,9 @@ class WarehouseResource:
             # Create warehouse
             self.db_conn.commit()
             
-            user_warehouse = UserWarehouse(warehouse_id=warehouse.warehouse_id, user_id=token.user_id, permission='rw')
+            user_warehouse = UserWarehouse(warehouse_id=warehouse.warehouse_id,
+                                           user_id=self.user_id, 
+                                           permission='rw')
 
             self.db_conn.add(user_warehouse)
 
@@ -103,3 +81,92 @@ class WarehouseResource:
 
         resp.media = {'success': 'Warehouse created successfuly'}
         resp.status = falcon.HTTP_201
+
+    def on_patch(self, req, resp, warehouse_id):
+        """
+        Handles PATCH requests.
+        
+        Allows editing warehouse entities fields.
+        
+        Args:
+            req ([type]): The request object.
+            resp ([type]): [description]The response object.
+        """
+        # Read warehouse details
+        name = req.media.get('name')
+        description = req.media.get('description')
+        latitude = req.media.get('latitude')
+        longitude = req.media.get('longitude')
+        
+        # If all details read are None
+        if not any([name, description, latitude, longitude]):
+            raise falcon.HTTPBadRequest('Bad Request', 'No warehouse parameters in request body.')
+
+        # Get the warehouse from the user
+        res = self.db_conn.query(Warehouse, UserWarehouse).join(UserWarehouse).filter(UserWarehouse.warehouse_id == warehouse_id).filter(UserWarehouse.user_id == self.user_id).first()
+
+        # Check if the warehouse does not exist
+        if res == None:
+            raise falcon.HTTPNotFound(title='Not Found', description='Requested entity was not found.')
+
+        warehouse = res[0]
+        user_warehouse = res[1]
+
+        # If user does not have write permission
+        if 'w' not in user_warehouse.permission:
+            raise falcon.HTTPForbidden('Forbidden', 'You do not have enough permissions for this action.')
+
+        # Edit warehouse parameters
+        if name != None:
+            warehouse.name = name
+
+        if description != None:
+            warehouse.description = description
+
+        if longitude != None:
+            warehouse.longitude = longitude
+
+        if latitude != None:
+            warehouse.latitude = latitude
+
+        # Add transaction
+        self.db_conn.add(warehouse)
+
+        # Commit modifications
+        try:
+            self.db_conn.commit()
+
+        except Exception:
+
+            # Rollback Changes
+            self.db_conn.rollback()
+            raise falcon.HTTPInternalServerError('Internal Server Error', 'An error ocurred, please inform the development team.')
+
+        resp.media = {'success': 'Warehouse modified successfuly'}
+        resp.status = falcon.HTTP_200
+
+    def on_delete(self, req, resp, warehouse_id):
+        """
+        Handles DELETE requests.
+        
+        Allows deleting warehouse entities.
+        
+        Args:
+            req ([type]): The request object.
+            resp ([type]): [description]The response object.
+        """
+        # Deletes warehouse from the current user account
+        self.db_conn.query(UserWarehouse).filter_by(warehouse_id=warehouse_id, user_id=self.user_id).delete()
+
+        # Commit modifications
+        try:
+            self.db_conn.commit()
+
+        except Exception:
+
+            # Rollback Changes
+            self.db_conn.rollback()
+            raise falcon.HTTPInternalServerError('Internal Server Error', 'An error ocurred, please inform the development team.')
+
+        resp.media = {'success': 'Warehouse deleted successfuly'}
+        resp.status = falcon.HTTP_200
