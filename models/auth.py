45e1c0a4b7269208875e229426cc92b5b793003a
rtista <workingtitus@gmail.com>
2019-04-19 00:36:04 +0100

    Upgraded sqlalchemy version although affected code is not used.

--- models/auth.py
+++ models/auth.py
@@ -15,7 +15,7 @@ class AuthToken(Base):
 
     __tablename__ = 'auth_token'
 
-    user_id = Column('user_id', Integer, ForeignKey("user.user_id"), primary_key=True, nullable=False)
+    user_id = Column('user_id', Integer, ForeignKey('user.user_id'), primary_key=True, nullable=False)
     auth_type = Column('auth_type', Enum('bearer'), nullable=False)
     token = Column('token', String(32), unique=True, nullable=False)
     expires_at = Column('expires_at', Integer, default=int(time() + AppConfig.TOKEN_LIFE))
